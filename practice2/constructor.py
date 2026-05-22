ec6ac1fd80083d23a209f4615046193a40cca6b3
Pavani Aluri <pavanialuri1775@gmail.com>
2026-05-22 22:41:37 +0530

    Added constructor problems

--- practice2/constructor.py
+++ practice2/constructor.py
@@ -10,3 +10,40 @@ class Employee:
         print('saraly:',self.salary)
 e=Employee(6609,"pavani",50000)
 e.display()
+
+#5
+class BankAccount:
+    def __init__(self,name,balance):
+        self.name=name
+        self.balance=balance
+    def deposit(self,amount):
+        if amount>0:
+            self.balance+=amount
+        else:
+            print("deposit must be positive")
+    def withdraw(self,amount):
+        if amount<=self.balance:
+            self.balance-=amount
+        else:
+            print("withdraw impossible")
+    def check_balance(self):
+        return f'{self.name} has {self.balance}'
+b=BankAccount("pavani",50000)
+b.deposit(20000)
+b.withdraw(10000)
+print(b.check_balance())
+
+#6  Car Information
+class Car:
+    def __init__(self,company,model,year):
+        self.company=company
+        self.model=model
+        self.year=year
+    def car_info(self):
+        print("comapny:",self.company)
+        print("model:",self.model)
+        print("year",self.year)
+c=Car("Tesla","Tesla Model 3",2020)
+c.car_info()
+
+        
