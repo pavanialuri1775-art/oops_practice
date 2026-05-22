397bbffd92cdffa024df179811085f2ef9f61d98
Pavani Aluri <pavanialuri1775@gmail.com>
2026-05-22 16:44:44 +0530

    Added public

---
+++ practice1/public.py
@@ -0,0 +1,21 @@
+#A protected variable is a variable that is:
+#intended to be accessed within the class and its subclasses
+#it is written using:_single_underscore
+class  Person:
+    def __init__(self):
+        self._age=25
+class Student(Person):
+    def display(self):
+        print(self._age)
+s=Student()
+s.display()#25
+
+#2
+class Vehicle:
+    def __init__(self):
+        self._speed=160
+class Car(Vehicle):
+    def display_speed(self):
+        print(self._speed)
+c=Car()
+c.display_speed()
